1948ae420f27917994932716b102edb8e98cf7ca
Claas Lorenz <clorenz@genua.de>
2021-05-21 17:20:20 +0200

    Exclude intermediate port and add some clarifications.

--- create_policy.py
+++ create_policy.py
@@ -11,8 +11,6 @@ with_paths = config_json.get("with_paths", False)
 tables = config_json["tables"]
 hdr_len = config_json["length"]
 
-commands = []
-
 table_out_json = {
     table : json.load(open("%s/%s.out.rules.json" % (src_dir, table))) for table in tables
 }
@@ -24,7 +22,7 @@ table_in_json = {
 topology_json = json.load(open("%s/topology.json" % src_dir))
 
 table_out_ports = {
-    table : [p for p in table_out_json[table]['ports'] if p != tid*100000+20000 and p < tid*100000+30000] for tid, table in enumerate(tables, start=1)
+    table : [p for p in table_out_json[table]['ports'] if p not in [tid*100000+10000, tid*100000+20000] and p < tid*100000+30000] for tid, table in enumerate(tables, start=1)
 }
 
 table_in_ports = {
@@ -35,9 +33,9 @@ first_in_port = {
     table : table_in_ports[table][0] for table in tables
 }
 
-probes = {
+probes = { # creates n probes per table, one for each source
     table : [
-        (10000+tid*100+pid, 10000+tid*100+pid) for pid in range(1, len(tables)+1)
+        (10000+tid*100+sid, 10000+tid*100+sid) for sid in range(1, len(tables)+1)
     ] for tid, table in enumerate(tables, start=1)
 }
 sources = {
@@ -45,16 +43,15 @@ sources = {
 }
 
 # add and connect probes
-source_commands = []
-for tid, table in enumerate(tables, start=1):
-    table_ports = table_out_ports[table]
-    table_probes = probes[table]
+probe_adds = []
+probe_links = []
+for probe_table in tables:
+    table_ports = table_out_ports[probe_table]
+    table_probes = probes[probe_table]
 
-    for table, table_probe in zip(tables, table_probes):
-        _sid, source_port = sources[table]
-        source_port = first_in_port[table]
+    for source_table, table_probe in zip(tables, table_probes):
         pid, probe_port = table_probe
-        source_commands.append({
+        probe_adds.append({
             "method" : "add_source_probe",
             "params" : {
                 "filter" : { "type" : "true" },
@@ -66,18 +63,18 @@ for tid, table in enumerate(tables, start=1):
                     "pathlets" : [
                         {
                             "type" : "last_ports",
-                            "ports" : [ source_port ]
+                            "ports" : [ first_in_port[source_table] ]
                         }
                     ]
 				} if with_paths else { "type" : "true" },
                 "mode" : "existential",
                 "id" : pid
-            },
+            }
         })
 
         # connect all egress ports to probe
-        for table_port in table_out_ports[table]:
-            source_commands.append({
+        for table_port in table_ports:
+            probe_links.append({
                 "method" : "add_link",
                 "params" : {
                     "from_port" : table_port,
@@ -90,17 +87,18 @@ for tid, table in enumerate(tables, start=1):
 
 
 # add and connect sources
-for tid, table in enumerate(tables, start=1):
-    table_ports = table_in_ports[table]
+source_links = []
+source_adds = []
+for table in tables:
     sid, source_port = sources[table]
 
-    commands.append({
+    source_adds.append({
 		"method" : "add_source",
 		"params" :
 		{
 			"hs" : {
                 "list" : [ ','.join(["xxxxxxxx"]*hdr_len) ],
-                "diff" : []
+                "diff" : None
             },
             "ports" : [ source_port ],
             "id" : sid
@@ -109,7 +107,7 @@ for tid, table in enumerate(tables, start=1):
 
     # connect source to first ingress port
     table_port = first_in_port[table]
-    commands.append({
+    source_links.append({
         "method" : "add_link",
         "params" : {
             "from_port" : source_port,
@@ -117,6 +115,6 @@ for tid, table in enumerate(tables, start=1):
         }
     })
 
-commands.extend(source_commands)
+commands = probe_adds + probe_links + source_adds + source_links
 
 json.dump({ 'commands' : commands }, open("%s/policy.json" % src_dir, 'w'), indent=2)
